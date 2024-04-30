a82cba04d737d3c58b387bd7735f7c09c732e1fe
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-04-29 17:52:53 -0700

    embed form widget

---
+++ components/TypeForm-Widget.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { Widget } from '@typeform/embed-react';
+
+
+const FormWidget = () => {
+    return <Widget id="vBOEFE8R" style={{ width: '100vw', height: '100vh'}} className="my-form" />;
+};
+
+export default FormWidget;
