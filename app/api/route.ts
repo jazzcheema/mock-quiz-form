49d3d8724ad27e543a9c87bf761efc1e9c610209
Jazz Cheema <jaskamalc@gmail.com>
2024-04-30 12:13:40 -0700

    worked on returning API from webhook and implementing ngrok to create a secure local tunnel connection

---
+++ app/api/route.ts
@@ -0,0 +1,19 @@
+// app/api/route.ts
+
+/** Webhook from Typeform. */
+export async function POST(request: Request) {
+  try {
+    const text = await request.text();
+    console.log("Received webhook data:", text);
+
+    return new Response("Webhook processed successfully!", {
+      status: 200,
+    });
+  } catch (error: any) {
+    console.log("Error in webhook:", error.message);
+
+    return new Response(`Webhook error: ${error.message}`, {
+      status: 400,
+    });
+  }
+}
